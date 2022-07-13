bd8b8aab44c5045877ecaff4b71a3c2a6b2d8cbc
Ratmir <nizameevratmir7@gmail.com>
2022-07-13 11:41:07 +0300

    upgrade product version

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,17 +1,17 @@
+import imp
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
-from backend.views import user,screenlist
+from django.urls import path
+from backend.views import user,screenlist,frontend
 
-router = routers.DefaultRouter()
-router.register(r'users', user.UserViewSet)
-router.register(r'screenlist', screenlist.ScreenlistViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', user.UserViewSet)
+# router.register(r'screenlist', screenlist.ScreenlistViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='api-auth')),
-    path('screenlist/', include('rest_framework.urls', namespace='screenlist'))
+    path('', frontend.front, name="front"),
+    # path('user/',user.UserViewSet.as_view(), name="user"),
+    path('screenlist/', screenlist.ScreenlistViewSet.screen_list, name="screenlist"),
 ]
--- backend/backend/views/screenlist.py
+++ backend/backend/views/screenlist.py
@@ -1,12 +1,28 @@
-from backend.models.screenlist import Screenlist
+from rest_framework import status
+from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # from rest_framework import permissions
+
+from backend.models.screenlist import Screenlist
 from backend.serializers.screenlist import ScreenlistSerializer
 
 class ScreenlistViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = Screenlist.objects.all()
     serializer_class = ScreenlistSerializer
     # permission_classes = [permissions.IsAuthenticated]
+
+    @api_view(['GET', 'POST'])
+    def screen_list(request):
+        if request.method == 'GET':
+            screenlist = Screenlist.objects.all()
+            serializer = ScreenlistSerializer(screenlist, many=True)
+            return Response(serializer.data)
+
+        elif request.method == 'POST':
+            serializer = ScreenlistSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
